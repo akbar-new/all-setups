c4bc148e88700ca85743037d73999cff5db5c0ee
akbar-new <munnaakbar1997@gmail.com>
2025-05-24 17:38:58 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,4 +1,4 @@
-amazon-linux-extras install java-openjdk11 -y
+yum install java-17-amazon-cotrretto -y
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v11.0.7/bin/apache-tomcat-11.0.7.tar.gz
 tar -zxvf apache-tomcat-11.0.7.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.7/conf/tomcat-users.xml
